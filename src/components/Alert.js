8c5e459c2ff32b6d2e043bd315d07af138534322
elsteramps <elstermetalhead@gmail.com>
2022-03-05 19:05:30 +0100

    some changes to components

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -8,12 +8,12 @@ function Alert() {
     if(!alert.visible){
         return null
     }
+    
     return (
-        <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`}>
-            <strong>Holy guacamole!</strong> 
+        <div className={`alert alert-${alert.type || 'warning'} alert-dismissible fade show`}>
             {alert.text}
-            <button onClick={hide} type="button" className="close" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
+            <button onClick={hide} type="button" className="btn btn-outline-success btn-sm" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
             </button>
       </div>
     );
